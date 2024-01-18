1348c1301c1d0f23451e2550b2713125c7c9073d
Jared Hanson <jred0011@gmail.com>
2024-01-18 11:57:21 -0800

    Test with all results

--- packages/tests/cypress/e2e/01-index-200.spec.cy.ts
+++ packages/tests/cypress/e2e/01-index-200.spec.cy.ts
@@ -1,4 +1,7 @@
 describe("Initial testing", () => {
+  it("failes loading index", () => {
+    cy.request("/api/accounts").then((resp) => expect(resp.status).to.eq(200));
+  });
   it("successfully loads index", () => {
     cy.visit("/");
   });
